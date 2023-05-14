63d20c2e233f08eb0418a95863cc0843fb089d56
Ƨ1ƦƊƳƝ0 <133577286+S1rDyn0@users.noreply.github.com>
2023-05-14 15:19:10 +0100

    Update DeSophos.bat

--- DeSophos.bat
+++ DeSophos.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 rem Crack Script Made by S1rDyn0
 cls
 echo ^|==============================^|
-echo ^|  DESOPHOS BY S1rDyn0  ^|
+echo ^|  DESOPHOS BY S1rDyn0         ^|
 echo ^|                              ^|
 echo ^|  Version             23.5.1  ^|
 echo ^|==============================^| & echo: & echo:
